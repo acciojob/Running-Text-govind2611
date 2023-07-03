1b6e640d32b115a7987b6069eb86f96e12e64625
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-03 09:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-
 const text = document.getElementById('text');
-      const speedInput = document.getElementById('speed');
-      const originalText = 'We love Programming!';
-      let currentIndex = 0;
+const speedInput = document.getElementById('speed');
+const originalText = 'We love Programming!';
+let currentIndex = 0;
 
-      function typeNextCharacter() {
-        if (currentIndex < originalText.length) {
-          const currentCharacter = originalText.charAt(currentIndex);
-          text.innerHTML += currentCharacter;
-          currentIndex++;
-          setTimeout(typeNextCharacter, 500 / parseInt(speedInput.value));
-        }
-      }
+// Set initial text content
+text.innerHTML = originalText;
 
-      speedInput.addEventListener('input', () => {
-        // Reset the text and index when the speed changes
-        text.innerHTML = '';
-        currentIndex = 0;
-        // Start typing again with the new speed
-        typeNextCharacter();
-      });
+function typeNextCharacter() {
+  if (currentIndex < originalText.length) {
+    const currentCharacter = originalText.charAt(currentIndex);
+    text.innerHTML = originalText.substring(0, currentIndex) + currentCharacter;
+    currentIndex++;
+    setTimeout(typeNextCharacter, 500 / parseInt(speedInput.value));
+  }
+}
 
-      // Start typing the text with the initial speed
-      typeNextCharacter();
-   
+speedInput.addEventListener('input', () => {
+  // Reset the index when the speed changes
+  currentIndex = 0;
+  // Start typing again with the new speed
+  typeNextCharacter();
+});
+
+// Start typing the text with the initial speed
+typeNextCharacter();
